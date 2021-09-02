886b3536887fe2f1969731a51e995ad75aeadca1
孙诚 <chengg.sun@ecovacs.com>
2021-09-02 11:05:23 +0800

    fix error

--- src/Middleware/Aspire.Middleware/Aspire/MiddlewareHelper.cs
+++ src/Middleware/Aspire.Middleware/Aspire/MiddlewareHelper.cs
@@ -3,18 +3,28 @@ using Microsoft.AspNetCore.Mvc.Controllers;
 
 namespace Aspire;
 
+/// <summary>
+/// 中间件帮助类.
+/// </summary>
 public static class MiddlewareHelper
 {
-    public static (string, string) GetControllerAndActionName(this HttpContext httpContext)
+    /// <summary>
+    /// 根据 <see cref="HttpContext"/> 获取 Controller 和 Action 的名称.
+    /// </summary>
+    /// <param name="httpContext"><see cref="HttpContext"/>.</param>
+    /// <returns>Controller 和 Action 的名称.</returns>
+    public static (string controllerName, string actionName) GetControllerAndActionName(this HttpContext httpContext)
     {
         var controllerActionDescriptor = httpContext
             .GetEndpoint()?
             .Metadata
             .GetMetadata<ControllerActionDescriptor>();
 
-        if (controllerActionDescriptor == null) return;
+        if (controllerActionDescriptor == null) return (string.Empty, string.Empty);
 
         var controllerName = controllerActionDescriptor.ControllerTypeInfo.Name;
         var actionName = controllerActionDescriptor.MethodInfo.Name;
+
+        return (controllerName, actionName);
     }
 }
